cb6fe33ce5bf3b9a09bc0c050d9fa6d9acc35afc
unknown <ante.dujic0506@gmail.com>
2021-02-08 20:38:35 +0000

    Week03_lab 3.3.x

---
+++ Week 03/lab 3.3.1 len.py
@@ -0,0 +1,7 @@
+# Program that reads in the string and outputs how long it is
+# Author: Ante Dujic
+
+inputString = input ("Enter a string: ")
+lengthOfString = len (inputString)
+
+print ("The length of {} is {}" .format (inputString, lengthOfString))
