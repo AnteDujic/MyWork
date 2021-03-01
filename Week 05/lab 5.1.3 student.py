294a81a7e44ac809d3d42d9ed583fab342e287ed
unknown <ante.dujic0506@gmail.com>
2021-03-01 11:03:23 +0000

    Week05_lab 5.1.3

---
+++ Week 05/lab 5.1.3 student.py
@@ -0,0 +1,21 @@
+# Program that stores a student name and list of courses and grades in a dict
+    # It then prints the data
+# Author: Ante Dujic
+
+
+student = {
+"name":"Mary",
+"modules": [
+{
+"courseName":"Programming",
+"grade": 45
+},
+{
+"courseName":"History",
+"grade":99
+}
+]
+}
+print ("Student: {}" .format(student["name"]))
+for module in student["modules"]:
+    print ("\t {} \t: {}" .format(module["courseName"], module["grade"]))
